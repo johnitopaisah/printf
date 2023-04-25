cabf47941be9ed672b732bbfd444aba926a3dc87
johnitopaisah <johnitopaisah@gmail.com>
2023-04-25 12:17:59 +0100

    Handle the following custom conversion specifiers:
    b: the unsigned int argument is converted to binary

--- print_binary.c
+++ print_binary.c
@@ -8,11 +8,19 @@
  */
 int print_binary(unsigned int n)
 {
-	int count = 0;
+	int i, count = 0;
+	int bit;
+	int leading_zero = 1;
 
-	if (n > 1)
-		count += print_binary(n / 2);
-	count += _putchar((n % 2) + '0');
+	for (i = sizeof(n) * 8; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+
+		if (bit == 0 && leading_zero)
+			continue;
+		leading_zero = 0;
+		count += _putchar(bit + '0');
+	}
 
 	return (count);
 }
